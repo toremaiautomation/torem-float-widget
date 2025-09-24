22a2824206790a8fd6327ad0eb1da10c0f46e6aa
toremaiautomation <toremaiautomation@gmail.com>
2025-09-24 13:03:59 -0500

    Update embed-chat.js

--- public/embed-chat.js
+++ public/embed-chat.js
@@ -6,9 +6,9 @@
   chatBtn.id = "torem-widget";
 
   // Logo image inside button
-chatBtn.innerHTML = '<img src="https://torem-float-widget.vercel.app/Image.png" alt="Chat" style="width: 48px; height: 48px; border-radius: 50%; object-fit: cover;" />';  
-  Object.assign(chatBtn.style, {
-    position: "fixed",
+chatBtn.innerHTML = '<img src="https://torem-float-widget.vercel.app/Image.png" alt="Chat" style="width: 54px; height: 54px; border-radius: 50%; object-fit: cover;" />';  Object.assign(chatBtn.style, {
+
+  position: "fixed",
     bottom: "20px",
     right: "20px",
     width: "60px",
